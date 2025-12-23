64c3a708562a505014afdd6c6ee4a8eeb46d889d
kenzierivan <kenzierivan261@gmail.com>
2025-12-23 22:24:13 +0800

    Implement basic Chirpy server with app, metrics, and admin endpoints
    - Serve main Chirpy web app at /app
    - Add readiness health check endpoint
    - Track and expose visit metrics
    - Add /admin/metrics HTML dashboard with visit count
    - Add /admin/reset endpoint to reset metrics
    - Organize routes under /api and /admin namespaces

--- main.go
+++ main.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
-	fmt.Println("hello world")
+	const filepathRoot = "."
+	const port = "8080"
+	apiCfg := &apiConfig{}
+	mux := http.NewServeMux()
+	mux.Handle(
+		"/app/", 
+		apiCfg.middlewareMetricsInc(
+			http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))),
+		),
+	)
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
+	
+	srv := &http.Server{
+		Addr: ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
 }
+
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, req *http.Request) {
+	w.Header().Add("Content-Type", "text/html")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf(`
+	<html>
+  		<body>
+    		<h1>Welcome, Chirpy Admin</h1>
+    		<p>Chirpy has been visited %d times!</p>
+  		</body>
+	</html>
+	`, cfg.fileserverHits.Load()),
+	))
+}
+
